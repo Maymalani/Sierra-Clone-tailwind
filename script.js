6d18adede11f30951033d4945b78e3c289378a6a
Mayank Malani <mayankmalani0@gmail.com>
2024-07-24 19:56:16 +0530

    first-commit

---
+++ script.js
@@ -0,0 +1,15 @@
+// var nav = document.getElementById('navHeader');
+// var prevScrollPos = window.scrollY;
+
+// const scrollFun = () => {
+//     var currentScrollPos = window.scrollY;
+//     if (prevScrollPos > currentScrollPos) {
+//         nav.style.display = 'block';
+//     } else {
+//         nav.style.display = 'none'
+//         nav.style.top = '0'
+//     }
+//     prevScrollPos = currentScrollPos;
+// };
+
+// window.addEventListener('scroll', scrollFun);
